f306982ce8c9fec0d8bb52dd570320df7f7c1c57
andrioc <andrio@rodrigo.silva@hotmail.com>
2021-03-25 17:01:43 -0300

    New adjustments and Mobile First

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import { Container } from './styles'
+import { 
+    Container,
+    Content 
+} from './styles'
+
+import imgBottom from '../../assets/images/bg-pattern-mobile.svg'
+import arrobaImg from '../../assets/images/illustration-box-desktop.svg'
+import imgWoman from '../../assets/images/illustration-woman-online-mobile.svg'
 
 const Home:React.FC = () => {
     return (
         <Container>
-            <h1>Home</h1>
+            <Content>
+                <img src={imgWoman} alt="" className="img-woman"/>
+                <img src={imgBottom} alt="" className="img-bottom"/>
+            </Content>
         </Container>
     )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,9 +1,39 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-width: 100%;
-height: 100vh;
-background: rgb(176,104,233);
-background: linear-gradient(180deg, rgba(176,104,233,1) 0%, rgba(100,99,231,1) 100%);
+    width: 100%;
+    height: 100vh;
+  
+  @media (max-width: 900px){
+      display: flex;
+      align-items: center;
+      justify-content: center;
+  }
+
+`;
+
+export const Content = styled.div`
+   display: flex;
+   align-items: center;
+   flex-direction: column;
+   background: #FFF;
+   width: 327px;
+   height: 535px;
+   border-radius: 23px;
+   margin-top: 148px;
+
+   /* Mobile First */
+   @media (max-width: 900px){
+
+      img.img-woman{
+         margin-top: -108px;
+      }
+
+      img.img-bottom{
+         margin-top: -90px;
+         margin-left: 30px;
+      }
+
+   }
 
 `;
